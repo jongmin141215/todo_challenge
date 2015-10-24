4d9f8c207bfd370bb23388b5f249a62431936330
Jongmin Kim <kjm141215@gmail.com>
2015-10-24 23:46:46 +0100

    Add id numbers to buttons & checkboxes

--- test/e2e/toDoListFeature.js
+++ test/e2e/toDoListFeature.js
@@ -2,10 +2,12 @@ describe('TodoList', function() {
 
   var taskInput = element(by.model('listCtrl.taskName'))
   var descriptionInput = element(by.model('listCtrl.taskDescription'))
-  var button = element(by.className('btn'))
+  var button = element(by.className('save'))
   var tasks = element.all(by.repeater('task in listCtrl.tasks'))
-  var checkBox = element(by.className('checkbox'))
-  var deleteButton = element(by.className('delete'))
+  var checkBox = element(by.id('checkbox0'))
+  var checkBox1 = element(by.id('checkbox1'))
+  var checkBox2 = element(by.id('checkbox2'))
+  var deleteButton = element(by.id('delete0'))
   beforeEach(function() {
     browser.get('http://localhost:8080');
   })
@@ -35,11 +37,28 @@ describe('TodoList', function() {
     expect(tasks.getAttribute('class')).toMatch('completed');
   })
 
-  it('can delete tasks', function() {
+  it('can delete a task', function() {
     taskInput.sendKeys('Weekend challenge');
     descriptionInput.sendKeys('Creating todoList using AngularJS');
     button.click();
     deleteButton.click();
     expect(tasks.getText()).toEqual([]);
   })
+
+  it('can only display not-completed tasks', function() {
+    taskInput.sendKeys('Weekend challenge');
+    button.click();
+    taskInput.sendKeys('Calling my mom');
+    button.click();
+    taskInput.sendKeys('Buying mangos');
+    button.click();
+    taskInput.sendKeys('Buying bodywash');
+    button.click();
+    checkBox1.click();
+    checkBox2.click();
+
+
+  })
+
+
 })
